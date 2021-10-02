e2eca0474b9aab07704f3149630952b965a5b382
Alex Omelnitckii <omelniz@gmail.com>
2021-10-02 20:12:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,34 @@ export function testReducer(reducer, initialState) {
     return new testReducer(reducer, initialState);
   }
 
-  this.state = reducer(initialState, { type: "MOCK_ACTION" });
+  this.state = reducer(initialState, {type: 'MOCK_ACTION'});
 
-  this.put = function(action) {
+  this.applyActions = function (actions = []) {
+    actions.forEach((action) => {
+      this.put(action);
+    });
+
+    return this;
+  };
+
+  this.put = function (action) {
     this.state = reducer(this.state, action);
     return this;
   };
 
-  this.expect = function(selector, value) {
+  this.expect = function (selector, value) {
     expect(selector(this.state)).toEqual(value);
     return this;
+  };
+
+  this.test = function (label, callback) {
+    if (!label) throw new Error('Provide label attribute');
+
+    callback(this.state);
+    return this;
+  };
+
+  this.getState = function () {
+    return this.state;
   };
 }
